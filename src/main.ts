9762a67b4a65e5c2835b77b706c8b86d84cf5776
jimluo123 <saulgoodmen@163.com>
2023-04-24 10:48:03 +0800

    transform

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { Renderer } from "./renderer";
+
+const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
+  document.getElementById("gfx-main")
+);
+
+const renderer = new Renderer(canvas);
+
+renderer.Initialize();
